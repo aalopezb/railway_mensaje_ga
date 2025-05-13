4322e00ce7252b442a4831e2f2610de9cd664526
aalopezb <a.alexander.l.b@gmail.com>
2025-05-12 22:31:36 -0500

    first commit

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/mensaje", methods=["GET"])
+def mensaje():
+    return jsonify({"mensaje": "Hola, ¿cómo estás?"})
+
+@app.route("/", methods=["GET"])
+def home():
+    return "Microservicio de mensaje en Railway."
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5003)
