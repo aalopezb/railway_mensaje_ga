beb449c857376166156d1882b9e7ec33e9054e64
aalopezb <a.alexander.l.b@gmail.com>
2025-05-12 23:04:03 -0500

    feat: :bug: Changes the ports 5000

--- Dockerfile
+++ Dockerfile
@@ -7,6 +7,6 @@ COPY . .
 
 RUN pip install --no-cache-dir -r requirements.txt
 
-EXPOSE 5001
+EXPOSE 5000
 
 CMD ["python", "app.py"]
--- app.py
+++ app.py
@@ -11,4 +11,4 @@ def home():
     return "Microservicio de mensaje en Railway."
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5001)
+    app.run(host="0.0.0.0", port=5000)
